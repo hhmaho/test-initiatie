35645876b3b050f50b300c440a633c577e8fe1e8
hhmaho <hhmaho@gmail.com>
2018-09-25 12:39:21 +0200

    startbestand leeg

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import {createStore} from 'redux'
+
+//initial state
+const initalState = {
+    test: "Startfase"
+}
+
+//reducer
+function reducer(state, action){
+    console.log()
+}
+
+//actions
+//store = reducer + initial state
